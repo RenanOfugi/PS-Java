317e36e44f9a92caebd76e65d6bbbe3fdeb1b01c
RenanOfugi <renan.ofugi@gmail.com>
2022-02-15 23:51:19 -0300

    refactor: adicionando anotações

--- src/main/java/br/com/supera/game/store/controller/GameStoreController.java
+++ src/main/java/br/com/supera/game/store/controller/GameStoreController.java
@@ -1,10 +1,16 @@
 package br.com.supera.game.store.controller;
 
+import br.com.supera.game.store.service.GameStoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/store")
+@RequiredArgsConstructor
 public class GameStoreController {
 
+    @Autowired
+    private final GameStoreService service;
 }
--- src/main/java/br/com/supera/game/store/repository/GameStoreRepository.java
+++ src/main/java/br/com/supera/game/store/repository/GameStoreRepository.java
@@ -1,4 +1,9 @@
 package br.com.supera.game.store.repository;
 
-public interface GameStoreRepository {
+import br.com.supera.game.store.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameStoreRepository extends JpaRepository<Product, Long> {
 }
