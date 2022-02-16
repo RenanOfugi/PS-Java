b4e53a8eee8b7e268d706d3c3624780de4cfd837
RenanOfugi <renan.ofugi@gmail.com>
2022-02-15 23:03:01 -0300

    refactor: adicionando anotações

--- src/main/java/br/com/supera/game/store/controller/GameStoreController.java
+++ src/main/java/br/com/supera/game/store/controller/GameStoreController.java
@@ -1,8 +1,10 @@
 package br.com.supera.game.store.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/store")
 public class GameStoreController {
-    
+
 }
