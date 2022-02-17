c43572ee62f8236e990615e31d06c2254f7c441b
RenanOfugi <renan.ofugi@gmail.com>
2022-02-17 12:18:27 -0300

    chore: apenas atualizações automaticas

--- src/main/java/br/com/supera/game/store/entity/Product.java
+++ src/main/java/br/com/supera/game/store/entity/Product.java
@@ -1,16 +1,10 @@
 package br.com.supera.game.store.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-@Getter
-@Setter
 @Entity
 public class Product {
 
