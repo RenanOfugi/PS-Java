5340e6c27dfa27cec621d8b996c69ad3fb9606d6
RenanOfugi <renan.ofugi@gmail.com>
2022-02-17 12:47:12 -0300

    feat: adicionado metodo get para listar todos os produtos disponiveis

--- src/main/java/br/com/supera/game/store/controller/GameStoreController.java
+++ src/main/java/br/com/supera/game/store/controller/GameStoreController.java
@@ -1,10 +1,15 @@
 package br.com.supera.game.store.controller;
 
+import br.com.supera.game.store.entity.Product;
 import br.com.supera.game.store.service.GameStoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/store")
@@ -13,4 +18,9 @@ public class GameStoreController {
 
     @Autowired
     private final GameStoreService service;
+
+    @GetMapping
+    public List<Product> listAllProducts(){
+        return service.listAll();
+    }
 }
