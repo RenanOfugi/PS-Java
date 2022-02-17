f81c617720177fe9bb69de07e827cc2a70d42b5e
RenanOfugi <renan.ofugi@gmail.com>
2022-02-17 11:55:11 -0300

    feat: implementado atributos do carrinho

--- src/main/java/br/com/supera/game/store/entity/Cart.java
+++ src/main/java/br/com/supera/game/store/entity/Cart.java
@@ -1,4 +1,24 @@
 package br.com.supera.game.store.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private BigDecimal subtotal;
+
+    private BigDecimal total;
+
+    private BigDecimal shipping_cost;
 }
