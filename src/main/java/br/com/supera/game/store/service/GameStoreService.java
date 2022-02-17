b5e3d93262dc2c4f177205d7dd6fbc8b06027f3d
RenanOfugi <renan.ofugi@gmail.com>
2022-02-17 12:48:13 -0300

    feat: implementado metodo de listagem de todos os produtos da loja

--- src/main/java/br/com/supera/game/store/service/GameStoreService.java
+++ src/main/java/br/com/supera/game/store/service/GameStoreService.java
@@ -1,7 +1,21 @@
 package br.com.supera.game.store.service;
 
+import br.com.supera.game.store.entity.Product;
+import br.com.supera.game.store.repository.GameStoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class GameStoreService {
+
+    @Autowired
+    private final GameStoreRepository repository;
+
+    public List<Product> listAll() {
+        return repository.findAll();
+    }
 }
