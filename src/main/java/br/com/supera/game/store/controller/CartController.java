08e33e87017fff37e0f392ba0781cff37ba94ed6
RenanOfugi <renan.ofugi@gmail.com>
2022-02-18 10:21:20 -0300

    feat: criado metodo para listar todos os produtos no carrinho

--- src/main/java/br/com/supera/game/store/controller/CartController.java
+++ src/main/java/br/com/supera/game/store/controller/CartController.java
@@ -1,13 +1,13 @@
 package br.com.supera.game.store.controller;
 
 import br.com.supera.game.store.entity.Cart;
+import br.com.supera.game.store.entity.Product;
 import br.com.supera.game.store.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/store/cart")
@@ -22,5 +22,10 @@ public class CartController {
 
         Cart cart = service.findById(1L); //Foi utilizado 1L referente ao 1 carrinho de compras, para o desafio
         return service.addToCart(id, cart);
+    }
+
+    @GetMapping
+    public List<Cart> listAllCart(){
+        return service.listAll();
     }
 }
