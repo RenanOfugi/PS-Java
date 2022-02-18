c122a8f21b8f21fce02350f0a956c3948161563b
RenanOfugi <renan.ofugi@gmail.com>
2022-02-18 00:05:17 -0300

    feat: implementado função de busca pelo id propriamente dito

--- src/main/java/br/com/supera/game/store/service/GameStoreService.java
+++ src/main/java/br/com/supera/game/store/service/GameStoreService.java
@@ -1,6 +1,7 @@
 package br.com.supera.game.store.service;
 
 import br.com.supera.game.store.entity.Product;
+import br.com.supera.game.store.exceptions.ProductNotFoundException;
 import br.com.supera.game.store.repository.GameStoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,9 @@ public class GameStoreService {
     public String createProduct(Product product) {
         Product productSave = repository.save(product);
         return "Produto " + productSave.id + " criado com sucesso";
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new ProductNotFoundException("id inexistente"));
     }
 }
