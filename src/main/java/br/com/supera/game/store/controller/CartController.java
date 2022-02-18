668dd5889a4e11f19a15d99e02c0b8dc7af2aa1a
RenanOfugi <renan.ofugi@gmail.com>
2022-02-18 00:00:03 -0300

    feat: implementado metodo controller de adicionar produtos no carrinho

--- src/main/java/br/com/supera/game/store/controller/CartController.java
+++ src/main/java/br/com/supera/game/store/controller/CartController.java
@@ -1,9 +1,23 @@
 package br.com.supera.game.store.controller;
 
+import br.com.supera.game.store.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/store/cart")
+@RequiredArgsConstructor
 public class CartController {
+
+    @Autowired
+    private final CartService service;
+
+    @PostMapping
+    public String addProduct(@RequestParam("id") Long id){
+        return service.addToCart(id);
+    }
 }
