b577ca7faca7631eead5da2158941a507eb64395
RenanOfugi <renan.ofugi@gmail.com>
2022-02-17 22:59:14 -0300

    refactor: adicionado @Repository e extendido JpaRepository

--- src/main/java/br/com/supera/game/store/repository/CartRepository.java
+++ src/main/java/br/com/supera/game/store/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package br.com.supera.game.store.repository;
 
-public class CartRepository {
+import br.com.supera.game.store.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
