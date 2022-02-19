57d22f44ba79f8b07b54b3170e4dd218b65f0801
RenanOfugi <renan.ofugi@gmail.com>
2022-02-18 23:54:40 -0300

    refactor: removendo codigo desnecessario

--- src/main/java/br/com/supera/game/store/exceptions/ProductExistException.java
+++ src/main/java/br/com/supera/game/store/exceptions/ProductExistException.java
@@ -9,6 +9,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ProductExistException extends RuntimeException {
     public ProductExistException(String message){
         super(message);
-        Logger.warn("");
     }
 }
