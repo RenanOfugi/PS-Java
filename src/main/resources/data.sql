cbf66e67969b94f1eea7b34bc7ba1e6bb84017af
RenanOfugi <renan.ofugi@gmail.com>
2022-02-19 11:29:15 -0300

    feat: criação dos arquivos de resources permitindo popular o banco de dados HSQLDB com dados no data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO  product (name, price, score, image) VALUES ('Super Mario Odyssey', 197.88, 100,'teste');
+INSERT INTO  product (name, price, score, image) VALUES ( 'Call Of Duty Infinite Warfare', 49.99, 80,'');
+INSERT INTO  product (name, price, score, image) VALUES ('The Witcher III Wild Hunt', 119.5, 250,'');
+INSERT INTO  product (name, price, score, image) VALUES ('Call Of Duty WWII', 249.99, 205,'');
+INSERT INTO  product (name, price, score, image) VALUES ('Mortal Kombat XL', 69.99, 150,'');
+INSERT INTO  product (name, price, score, image) VALUES ('Shards of Darkness', 74.94, 400,'');
+INSERT INTO  product (name, price, score, image) VALUES ('Terra Média: Sombras de Mordor', 79.99, 50,'');
+INSERT INTO  product (name, price, score, image) VALUES ('FIFA 18', 195.39, 325,'');
+INSERT INTO  product (name, price, score, image) VALUES ('Horizon Zero Dawn', 115.8, 290,'');
